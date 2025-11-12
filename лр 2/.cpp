ac45abd66655c7f625d2d1524c466b426c2bc41b
sonasamsonova222-cmd <sonasamsonova222@gmail.com>
2025-11-12 21:46:54 +0300

    Update .cpp

--- лр 2/.cpp
+++ лр 2/.cpp
@@ -1,1 +1,23 @@
+  **МУЛЬТИСПИСОК**
+struct Node {
+    int data;
+    Node *prev;
+    Node *next;
+};
+
+  **ОЧЕРЕДЬ**
+std::queue<std::string> queue;
+queue.push("я"); 
+queue.push("мы"); 
+queue.push("ты");
+
+  **ДЕК**
+std::deque<int> d = {9, 4, 6};
+
+  **ПРИОРИТЕТНАЯ ОЧЕРЕДЬ**
+std::priority_queue<int> pq;
+pq.push(1);     
+pq.push(27);    
+pq.push(14); 
+
 
