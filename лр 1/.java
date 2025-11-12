ec39309e07ccb297bdb5eaad81eeadff4768b128
sonasamsonova222-cmd <sonasamsonova222@gmail.com>
2025-11-12 20:39:51 +0300

    Update .java

--- лр 1/.java
+++ лр 1/.java
@@ -1,1 +1,26 @@
+     Java
+СОЗДАНИЕ СПИСКА:
+import java.util.ArrayList;
+import java.util.List;
 
+public class Main {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>(); 
+        list.add("1");                     
+        list.add("2");                     
+        System.out.println(list);               
+    }
+}
+
+СОЗДАНИЕ СТЕКА:
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Main {
+    public static void main(String[] args) {
+        Deque<String> stack = new ArrayDeque<>(); 
+        stack.push("1");                     
+        stack.push("2");                      
+        System.out.println(stack);                
+    }
+}
