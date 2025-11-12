dd76a4e605bcf2327e5d4b625fdefe60a2a2e9dd
sonasamsonova222-cmd <sonasamsonova222@gmail.com>
2025-11-12 20:43:40 +0300

    Update .cpp

--- лр 1/.cpp
+++ лр 1/.cpp
@@ -1,1 +1,25 @@
+   C++
+СОЗДАНИЕ МАССИВА:
+#include <iostream>
 
+int main() {
+    int arr[2];           
+    arr[0] = 10;         
+    arr[1] = 20;          
+
+    std::cout << arr[0] << " " << arr[1] << "\n"; 
+    return 0;
+}
+
+СОЗДАНИЕ СТЕКА:
+#include <iostream>
+#include <stack>
+
+int main() {
+    std::stack<std::string> st;
+    st.push("10");          
+    st.push("20");          
+
+    std::cout << st.top() << "\n"; 
+    return 0;
+}
